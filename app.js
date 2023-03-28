a4232e7a7ae6b02cc0b3dc290c55544e35cb14db
Rahilsaleem12 <rahilsaleem565@gmail.com>
2023-03-28 13:15:05 +0500

    some code

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 var emails = [];
 var passwords = [];
 
@@ -8,20 +11,23 @@ function signup() {
   passwords.push(signupPassword);
   console.log(signupEmail);
   console.log(signupPassword);
-  window.location.href = "./index2.html";
+  // window.location.href = "./index2.html";
 }
 
 function login() {
   var loginEmail = document.getElementById("inputemail2").value;
   var loginPassword = document.getElementById("inputpasword2").value;
+  var loginSuccessful = false; // flag variable
   for (var i = 0; i < emails.length; i++) {
     if (emails[i] == loginEmail && passwords[i] == loginPassword) {
       alert("Congratulations, login successful!");
-      return;  
+      window.location.href = "./index3.html";
+      loginSuccessful = true; // set flag variable to true
+      break; // exit the loop since login was successful
     }
-   else{
-     alert("Incorrect email or password, please try again.");
-     console.log("check condition")
-    }
+  }
+  if (!loginSuccessful) { // show alert if login was not successful
+    alert("Incorrect email or password, please try again.");
+    console.log("check condition");
   }
 }
