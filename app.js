9264a2e6f5d7cb5f4cc9334aca785169cb198da6
Rahilsaleem12 <rahilsaleem565@gmail.com>
2023-03-28 14:28:25 +0500

    some code

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// var emails = [];
+// var passwords = [];
+
+// function signup() {
+//   var signupEmail = document.getElementById("inputemail1").value;
+//   var signupPassword = document.getElementById("inputpasword1").value;
+//   emails.push(signupEmail);
+//   passwords.push(signupPassword);
+//   console.log(signupEmail);
+//   console.log(signupPassword);
+//   // window.location.href = "./index2.html";
+// }
+
+// function login() {
+//   var loginEmail = document.getElementById("inputemail2").value;
+//   var loginPassword = document.getElementById("inputpasword2").value;
+//   for (var i = 0; i < emails.length; i++) {
+//     if (emails[i] == loginEmail && passwords[i] == loginPassword) {
+//       alert("Congratulations, login successful!");
+//       window.location.href = "./index3.html"
+  
+//     }
+//    else{
+//      alert("Incorrect email or password, please try again.");
+//      console.log("check condition")
+//     }
+//   }
+// }
 
 
 
@@ -28,6 +56,5 @@ function login() {
   }
   if (!loginSuccessful) { // show alert if login was not successful
     alert("Incorrect email or password, please try again.");
-    console.log("check condition");
   }
 }
